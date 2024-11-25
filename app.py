792025dd90342cea37e202ad80aa3d19300ffcd6
Scott Zeta <scott-zeta@outlook.com>
2024-11-25 15:51:22 +1030

    retrieve thread

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import os
 from assistant_module import Assistant
 from dotenv import load_dotenv
 from thread_module import Thread
+from flask import Flask, session
 
 load_dotenv()
 
-if __name__ == '__main__':
-    assistant = Assistant()
-    
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+@app.route('/')
+def assist():
     thread = Thread()
     thread.create_retrieve_thread()
+    return f"Current Thread ID: {session.get('thread_id', 'No Thread Found')}"
+
+if __name__ == '__main__':
+    assistant = Assistant()
+
+    app.run(debug=False)
--- thread_module.py
+++ thread_module.py
@@ -1,18 +1,25 @@
 import openai
+from flask import session
 
 class Thread:
-    
-    thread_id = "thread_1LlpHgmuRcp77g8HWwVONCkX"
     
     def __init__(self) -> None:
         self.client = openai
         self.thread = None
         
     def create_retrieve_thread(self):
-        if Thread.thread_id:
-            self.thread = self.client.beta.threads.retrieve(Thread.thread_id)
-            print(self.thread)
+        if 'thread_id' in session:
+            try:
+                self.thread = self.client.beta.threads.retrieve(session['thread_id'])
+                print(f"Thread Retrieved: {self.thread}")
+            except Exception as e:
+                print(f"Session didn't found: {e}")
+                thread_entity = self.client.beta.threads.create()
+                session['thread_id'] = thread_entity.id
+                self.thread = thread_entity
+                print(f"Thread Created after not found: {self.thread}")
         else:
             thread_entity = self.client.beta.threads.create()
+            session['thread_id'] = thread_entity.id
             self.thread = thread_entity
-            print(self.thread)
+            print(f"Thread Created: {self.thread}")
